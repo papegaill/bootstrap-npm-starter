df68d5e214128f29757a3b484b27470b6cf67ba6
Alexis Gaillard <a@bypopcorn.com>
2020-08-26 18:09:07 +0200

    Add webpack for js compilation and change the file structure

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+// Importing JavaScript
+//
+// You have two choices for including Bootstrap's JS files—the whole thing,
+// or just the bits that you need.
+
+
+// Option 1
+//
+// Import Bootstrap's bundle (all of Bootstrap's JS)
+
+import "bootstrap";
+
+
+// Option 2
+//
+// Import just what we need
+
+// import "bootstrap/js/dist/alert.js";
+// import "bootstrap/js/dist/button.js";
+// import "bootstrap/js/dist/carousel.js";
+// import "bootstrap/js/dist/collapse.js";
+// import "bootstrap/js/dist/dropdown.js";
+// import "bootstrap/js/dist/modal.js";
+// import "bootstrap/js/dist/popover.js";
+// import "bootstrap/js/dist/scrollspy.js";
+// import "bootstrap/js/dist/tab.js";
+// import "bootstrap/js/dist/toast.js";
+// import "bootstrap/js/dist/tooltip.js";
